f9f7d011693450e2262e86f3cb9bd295ba238b44
Florin <User@192-168-0-103.rdsnet.ro>
2023-02-15 22:28:21 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+  //
     }
 }
