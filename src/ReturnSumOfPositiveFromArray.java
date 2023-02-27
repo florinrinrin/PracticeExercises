d0f9a28260e75426ce498aeabd4559847737cb57
Florin <User@MacBook-Pro.local>
2023-02-27 23:51:19 +0200

    returnSumOfPositive

---
+++ src/ReturnSumOfPositiveFromArray.java
@@ -0,0 +1,24 @@
+public class ReturnSumOfPositiveFromArray {
+    public static void main(String[] args) {
+        int[] arrayOfInt = {1, -2, 3, 4, 5};
+        int[] arrayOfInt1 = {-1, -2, 3, -4, 5};
+        int[] arrayOfInt2 = {1, -2, 3, -4, -5};
+        int[] arrayOfInt3 = {-1, -2, -3, -4, -5};
+        sum(arrayOfInt);
+        sum(arrayOfInt1);
+        sum(arrayOfInt2);
+        sum(arrayOfInt3);
+    }
+
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int j : arr) {
+            if (j >= 0) {
+                sum += j;
+            }
+        }
+        System.out.println(sum);
+        return Math.max(sum, 0);
+    }
+
+}
